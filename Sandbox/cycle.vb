73e593c9cf4df9000a270ae568760a16655229f4
unknownsaying <15368104568@189.cn>
2025-03-02 19:48:37 +0800

    Update cycle.vb

--- Sandbox/cycle.vb
+++ Sandbox/cycle.vb
@@ -1,36 +1,36 @@
 Imports BigAlpha.vb
-imports fx789101112.ts
+imports fw789101112.ts
 Dim 1 As Boolean True
 Function Cycle
 For(A = 0;A <= 1;A++)
 For(B = 0;B <= 1;B++)
 For(C = 0;C <= 1;C++)
 For(D = 0;D <= 1;D++)
-Goto fx7
+Goto fw7
 For(E = 0;E <= 1;E++)
 For(F = 0;F <= 1;F++)
 For(G = 0;G <= 1;G++)
 For(H = 0;H <= 1;H++)
-Goto fx8
+Goto fw8
 For(I = 0;I <= 1;I++)
 For(J = 0;J <= 1;J++)
 For(K = 0;K <= 1;K++)
 For(L = 0;L <= 1;L++)
-Goto fx9
+Goto fw9
 For(M = 0;M <= 1;M++)
 For(N = 0;N <= 1;N++)
 For(O = 0;O <= 1;O++)
 For(P = 0;P <= 1;P++)
-Goto fx10
+Goto fw10
 For(Q = 0;Q <= 1;Q++)
 For(R = 0;R <= 1;R++)
 For(S = 0;S <= 1;S++)
 For(T = 0;T <= 1;T++)
-Goto fx11
+Goto fw11
 For(U = 0;U <= 1;U++)
 For(V = 0;V <= 1;V++)
 For(W = 0;W <= 1;W++)
-Goto fx12
+Goto fw12
 For(X = 0;X <= 1;X++)
 For(Y = 0;Y <= 1;Y++)
 For(Z = 0;Z <= 1;Z++)
