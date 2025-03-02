c6707269d68a7ecd71667c7761107af6fc563e64
unknownsaying <15368104568@189.cn>
2025-03-02 19:48:01 +0800

    Update and rename fx789101112.ts to fw789101112.ts

---
+++ Sandbox/tsc/fw789101112.ts
@@ -0,0 +1,7 @@
+import "XYZ.ts"
+const fw7 = 'x1^7' + 'w1 * x1' + 'w2 * y1' + 'w3 * z1';
+const fw8 = "x1^7" + "w1' * x1" + "w2 * y1" + "w3 * z1";
+const fw9 = 'x2^7' + 'w1 * x2' + 'w2 * y2' + 'w3 * z1';
+const fw10 = "x2^7" + "w1' * x2" + "w2' * y2" + "w3' * z2"; 
+const fw11 = 'x3^7' + 'w1 * x3' + 'w2 * y3' + 'w3 * z3';
+const fw12 = "x3^7" + "w1' * x3" + "w2' * y3" + "w3' * z3";
--- Sandbox/tsc/fx789101112.ts
+++
@@ -1,7 +0,0 @@
-import "XYZ.ts"
-const fx7 = 'x1^7' + 'w1 * x1' + 'w2 * y1' + 'w3 * z1';
-const fx8 = "x1^7" + "w1' * x1" + "w2 * y1" + "w3 * z1";
-const fx9 = 'x2^7' + 'w1 * x2' + 'w2 * y2' + 'w3 * z1';
-const fx10 = "x2^7" + "w1' * x2" + "w2' * y2" + "w3' * z2"; 
-const fx11 = 'x3^7' + 'w1 * x3' + 'w2 * y3' + 'w3 * z3';
-const fx12 = "x3^7" + "w1' * x3" + "w2' * y3" + "w3' * z3";
