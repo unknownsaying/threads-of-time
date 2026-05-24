abffad8c192fd2a6b92cb92b3ad2092e348ac6e5
unknownsaying <15368104568@189.cn>
2026-05-24 16:53:36 +0800

    Update Coordinate.vb

--- Sandbox/Coordinate.vb
+++ Sandbox/Coordinate.vb
@@ -60,9 +60,9 @@ const r = Sqrt(X * X + Y * Y + Z * Z)
 const theta = Atan2(Y, X)
 const phi = Atan2(Sqrt(X * X + Y * Y), Z)
   
-Byval X = r * SIN(theta) * COS(phi)
-Byval Y = r * SIN(theta) * SIN(phi)
-Byval Z = r * COS(theta)
+Byref X = r * SIN(theta) * COS(phi)
+Byref Y = r * SIN(theta) * SIN(phi)
+Byref Z = r * COS(theta)
 Redim X ^ 2 + Y ^ 2 = Z ^ 2
 
 End Function
